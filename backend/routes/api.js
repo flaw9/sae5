e48d14492514bb0428460e00756810c80a986cb6
flaw9 <contact@flaw.dev>
2024-01-18 11:35:25 +0100

    get abonnements

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,4 +2,17 @@ const database = require('../modules/db.js');
 
 const router = require('express').Router();
 
+router.get('/abonnements', (req, res) => {
+    // Make sure we're logged in as a structure
+    if (!req.session.user) return res.status(401).send('Not logged in');
+    if (req.session.user.type !== 'structure') return res.status(403).send('Not a structure');
+
+    // Get the abonnements
+    const abonnements = database.query('SELECT * FROM abonnement WHERE structure = ?', [req.session.user.id]);
+    if (!abonnements) return res.status(500).send('Database error');
+
+    // Return the abonnements
+    return res.json(abonnements);
+});
+
 module.exports = router;
